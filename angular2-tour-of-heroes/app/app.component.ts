80566c995143012643579241f17e04c6d086bf98
Becky Moran <mary.moran@smartstream-stp.com>
2016-07-22 16:25:33 -0400

    Refactored the heroes detail out of the app component.  Added a service to inject the hero list data

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -15,15 +13,8 @@ export class Hero {
                    (click)="onSelect(hero)">
                     <span class="badge">{{hero.id}}</span> {{hero.name}}
                 </li>
-             </ul>
-             <div *ngIf="selectedHero">
-                 <h2>{{selectedHero.name}} details!</h2>
-                 <div><label>id: </label>{{selectedHero.id}}</div>
-                 <div>
-                 <label>name: </label>
-                 <input [(ngModel)]="selectedHero.name" placeholder="name">
-                 </div>
-             </div>`,
+             </ul> 
+             <my-hero-detail [hero]="selectedHero"></my-hero-detail>`,
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -71,27 +62,29 @@ export class Hero {
     height: 1.8em;
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
-  }`]
+  }`],
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
 
-export class AppComponent {
-  public heroes = HEROES;
+export class AppComponent implements OnInit {
+  public heroes: Hero[];
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  
-  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(){
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(){
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 }
 
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
